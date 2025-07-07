3d087a5483ee1f5e63bb668f7e5f0b6005d59602
Foodxz <Haithem@lexicon.se>
2025-07-07 11:09:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var ham = 0;
+var cheese = 0;
+
+//Report to HQ
+
+var report = 'blank';
+
+if(ham >= 10 && cheese >= 10){
+    report = "Strong sales of both items";
+}
+else if(ham === 0 && cheese === 0){
+    report = "Nothing sold";
+}
+else{
+    report = "We had some sales";
+}
+
+console.log(report);
